074441af0d092dff1c2a3633451f2f7ac955b3c9
Vincenzo-Vengeance <134833962+Vincenzo-Vengeance@users.noreply.github.com>
2025-01-05 01:05:26 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,11 @@
+valid_email = 'bochkova03@gmail.com'
+valid_password = 'Moment'
+
+not_valid_email = 'bochkova3@mail.com'
+not_valid_password = 'ERFi'
+
+vacant_email = ''
+vacant_password = ''
+
+empty_auth_key = {'key': ''}
+
